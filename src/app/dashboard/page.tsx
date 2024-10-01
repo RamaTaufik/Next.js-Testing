53ea6b16e1fb5e78336b43e62f7d4cee6c131a2a
unknown <ramata2447@gmail.com>
2024-10-01 12:41:57 +0700

    Base

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+    title: 'Dashboard',
+}
+
+export default function Home() {
+    return (
+        <div>Yatta</div>
+    )
+}
